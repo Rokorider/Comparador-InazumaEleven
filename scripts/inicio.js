5cb35eeb165cf51e0c9e3960a41c11c88f9bee1b
[Yilin Zhou] <yilin.zh135@gmail.com>
2024-04-22 17:27:16 +0200

    Cambiar colores de inicio

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -7,17 +7,25 @@ let indiceImagen = 0; // Índice de la imagen actual
 const imagenesFunciones = {
     "../img/fondo1.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#f59701";
-        // TODO
         // Pon aqui el codigo para cambiar los colores de la caja
+        document.querySelector(".cajaInicio").style.backgroundColor = "#006265";
+        document.querySelector(".boton").style.backgroundColor = "#e2550b";
     },
     "../img/fondo2.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#ffb831";
+        document.querySelector(".cajaInicio").style.backgroundColor = "#1264a3";
+        document.querySelector(".boton").style.backgroundColor = "#9d5735";
     },
     "../img/fondo3.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#fce13a";
+        document.querySelector(".cajaInicio").style.backgroundColor = "#297891";
+        document.querySelector(".boton").style.backgroundColor = "#3e3a3d";
     },
     "../img/fondo4.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#537d8c";
+        document.querySelector(".cajaInicio").style.backgroundColor = "#304547";
+        document.querySelector(".boton").style.backgroundColor = "#021a58";
+
     },
 };
 
@@ -34,7 +42,19 @@ document.addEventListener("keydown", function (event) {
             // Si no hay una función asociada a la imagen actual, eliminar cualquier estilo personalizado
             document.querySelector(".caja").style.backgroundColor = "";
         }
+    }else if(event.key === "ArrowDown"){
+        indiceImagen = (indiceImagen - 1 + imagenes.length) % imagenes.length; 
+        const imagenActual = imagenes[indiceImagen];
+        document.querySelector(".caja").style.backgroundImage = `url('${imagenActual}')`;
+
+    if (imagenesFunciones.hasOwnProperty(imagenActual)) {
+        imagenesFunciones[imagenActual]();
+    } else {
+        document.querySelector(".caja").style.backgroundColor = "";
     }
+    }
+
+    
 });
 
 btnInicioSesion.addEventListener('click', function () {
