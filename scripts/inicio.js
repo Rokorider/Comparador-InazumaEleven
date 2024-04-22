16e7f2f6bba978d2aeb404f90b0038acafe46f04
kiwan33 <rumansillar@gmail.com>
2024-04-22 14:21:02 +0200

    Actualizar página de inicio de sesión y estilos

---
+++ scripts/inicio.js
@@ -0,0 +1,7 @@
+let btnInicioSesion = document.getElementById('btnInicioSesion');
+
+btnInicioSesion.addEventListener('click', function() {
+    setTimeout(() => {
+        window.location.href = '../index.html';
+    }, 2000);
+});
