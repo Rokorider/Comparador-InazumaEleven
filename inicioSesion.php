2a02e0c06dcff68ca487def428499091fe360668
kiwan33 <rumansillar@gmail.com>
2024-04-23 15:28:06 +0200

    Actualizar funcionalidad de inicio de sesión y redirección en iniciosSesion.php

--- inicioSesion.php
+++ inicioSesion.php
@@ -1,11 +1,39 @@
 <?php
+
+// Credenciales de la base de datos
+$dbhost = "localhost"; // Servidor de la base de datos
+$dbuser = "ruben"; // Usuario de la base de datos
+$dbpassword = "1234"; // Contraseña de la base de datos
+$dbname = "apiinazuma"; // Nombre de la base de datos
+
+
+// Conexión a la base de datos
+$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+
+// Verificar la conexión
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Verificar si se han enviado usuario y contraseña en el post
+if (!isset($_POST['usuario']) || !isset($_POST['contrasena'])) {
+    die("Por favor, ingrese un usuario y contraseña");
+}
+
+// Obtener usuario y contraseña del post
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 
-if ($usuario == 'admin' && $contrasena == 'admin') {
-    echo 'Inicio de sesión exitoso como administrador';
-} else {
-    echo 'Inicio de sesión exitoso como usuario';
-}
+// Consulta a la base de datos
+$query = mysqli_query( $connection,  "SELECT * FROM usuarios WHERE nombreUsuario = '$usuario' AND contrasena = '$contrasena'");
 
-header('Location: index.html');
+// Comprobación
+// Si hay al menos un resultado
+if (mysqli_num_rows($query) > 0) { 
+    // Establecer una variable de sesión para indicar que el usuario ha iniciado sesión
+    session_start();
+    $_SESSION['usuario'] = $usuario;
+    header("Location: index.php");
+} else {
+    header("Location: iniciosSesion.php");
+}
--- scripts/inicio.js
+++ scripts/inicio.js
@@ -10,25 +10,25 @@ const imagenesFunciones = {
         // Pon aqui el codigo para cambiar los colores de la caja
         document.querySelector(".cajaInicio").style.backgroundColor = "#006265";
         document.querySelector(".boton").style.backgroundColor = "#e2550b";
-        document.querySelector(".registro2").style.color = "#b8371c";
+        document.querySelector(".registroEnlace").style.color = "#b8371c";
     },
     "../img/fondo2.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#ffb831";
         document.querySelector(".cajaInicio").style.backgroundColor = "#1264a3";
         document.querySelector(".boton").style.backgroundColor = "#9d5735";
-        document.querySelector(".registro2").style.color = "#7f8da2";
+        document.querySelector(".registroEnlace").style.color = "#7f8da2";
     },
     "../img/fondo3.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#fce13a";
         document.querySelector(".cajaInicio").style.backgroundColor = "#297891";
         document.querySelector(".boton").style.backgroundColor = "#3e3a3d";
-        document.querySelector(".registro2").style.color = "#a5c9d2";
+        document.querySelector(".registroEnlace").style.color = "#a5c9d2";
     },
     "../img/fondo4.png": function () {
         document.querySelector(".caja").style.backgroundColor = "#457e8e";
         document.querySelector(".cajaInicio").style.backgroundColor = "#304547";
         document.querySelector(".boton").style.backgroundColor = "#021a58";
-        document.querySelector(".registro2").style.color = "#537d8c";
+        document.querySelector(".registroEnlace").style.color = "#537d8c";
 
     },
 };
@@ -37,17 +37,6 @@ const imagenesFunciones = {
 let secuenciaFlechas = [];
 let numeroSecuencia = 0;
 
-// Obtener el elemento de audio
-let cancion = document.getElementById('cancion');
-// Función para reproducir la canción
-function reproducirCancion() {
-    cancion.play();
-}
-// Agregar un controlador de eventos de clic al elemento body
-document.body.addEventListener('click', function () {
-    // Reproducir la canción cuando se haga clic en cualquier parte del cuerpo
-    reproducirCancion();
-});
 let cancion2 = document.getElementById('cancion2');
 
 document.addEventListener("keydown", function (event) {
@@ -70,10 +59,8 @@ document.addEventListener("keydown", function (event) {
                     document.querySelector(".caja").style.backgroundColor = "#292929";
                     document.querySelector(".cajaInicio").style.backgroundColor = "#d93832";
                     document.querySelector(".boton").style.backgroundColor = "#292929";
-                    document.querySelector(".registro2").style.color = "black";
+                    document.querySelector(".registroEnlace").style.color = "black";
                     numeroSecuencia = 0; // Reiniciar el numeroSecuencia
-                    cancion.pause();
-                    cancion.currentTime = 0;
                     cancion2.play();
                 } else {
                     // Cambiar la imagen de fondo por otra que tengas en una variable
@@ -92,7 +79,7 @@ document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowUp") { // Si se presiona la flecha hacia arriba
         cancion2.pause();
         cancion2.currentTime = 0;
-        cancion.play();
+
         indiceImagen = (indiceImagen + 1) % imagenes.length; // Avanza al siguiente índice de imagen
         const imagenActual = imagenes[indiceImagen]; // Obtiene la URL de la imagen actual
         document.querySelector(".caja").style.backgroundImage = `url('${imagenActual}')`; // Cambia el fondo del div
@@ -107,7 +94,7 @@ document.addEventListener("keydown", function (event) {
     } else if (event.key === "ArrowDown") {
         cancion2.pause();
         cancion2.currentTime = 0;
-        cancion.play();
+
         indiceImagen = (indiceImagen - 1 + imagenes.length) % imagenes.length;
         const imagenActual = imagenes[indiceImagen];
         document.querySelector(".caja").style.backgroundImage = `url('${imagenActual}')`;
