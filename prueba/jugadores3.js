d72c4418c2507b124e564323b18f100b3a36fd6d
kiwan33 <rumansillar@gmail.com>
2024-04-25 18:27:58 +0200

    Revert "Actualizar estilos de jugadores3.css y jugadores3.js"
    This reverts commit bc211807c0abb2c07583b242a2bc7187f31a8ea0.

--- prueba/jugadores3.js
+++ prueba/jugadores3.js
@@ -9,7 +9,7 @@ function actualizarContenidoSegunPantalla() {
     let contenedorJugadores = document.getElementById('contenedorJugadores');
 
     // Verificar el ancho de la pantalla
-    if (anchoPantalla < 650) {
+    if (anchoPantalla < 425) {
         // Si el ancho de la pantalla es menor que 425px, borrar el contenido del section
         contenedorJugadores.innerHTML = `
             <div class="contenedorBuscadores">
