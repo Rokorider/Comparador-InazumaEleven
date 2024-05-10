03bdc9fb86a346a0605694e966ecc2afe8c5f2f3
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-05-10 11:55:38 +0200

    Nuevo input en el formulario, nuevo js

--- registro.php
+++ registro.php
@@ -1,4 +1,6 @@
 <?php
+
+
 // Credenciales de la base de datos
 $dbhost = "localhost"; // Servidor de la base de datos
 $dbuser = "comparador"; // Usuario de la base de datos
@@ -7,21 +9,29 @@ $dbname = "apiinazuma"; // Nombre de la base de datos
 
 // Conexión a la base de datos
 $connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+
 // Verificar la conexión
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// Verificar si se han enviado usuario y contraseña en el post
-if (!isset($_POST['usuario']) || !isset($_POST['contrasena'])) {
-    die("Por favor, ingrese un usuario y contraseña");
+
+// Verificar si se han enviado usuario, correo y contraseña en el post
+if (!isset($_POST['usuario']) || !isset($_POST['correo']) || !isset($_POST['contrasena'])) {
+    die("Por favor, ingrese un usuario, correo y contraseña");
 }
+
 // Obtener usuario y contraseña del post
 $usuario = $_POST['usuario'];
+$correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
 
-// Consulta a la base de datos$query = mysqli_query( $connection,  "INSERT INTO usuarios (usuario, contrasena) VALUES ('$user', '$password')");
+// Consulta a la base de datos
+$query = mysqli_query( $connection,  "SELECT * FROM usuarios WHERE nombre = '$usuario' AND correo = '$correo' AND contrasena = '$contrasena'");
+
+// Comprobación
+// Si hay al menos un resultado
 if ($query) {
-    header("Location: login.html");
+    header("Location: index.html");
 } else {
     header("Location: registro.html");
 }
