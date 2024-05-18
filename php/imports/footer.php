f43f06405dcb53a426c1a277fd9de95647e3ad2b
kiwan33 <rumansillar@gmail.com>
2024-05-18 21:13:29 +0200

    Actualizar estilos de footer y lógica para mostrar frases aleatorias

--- php/imports/footer.php
+++ php/imports/footer.php
@@ -1,21 +1,21 @@
 <footer>
-        <div class="footer">
-            <div class="logoFooter">
-                <a href="#"><img src="../img/logo/InazumaElevenComparadorLogo2.png" alt="Logo Inazuma Comparador" class="logoImg"></a>
-            </div>
-            <div class="textoFooter">
-                <p>"¡Con determinación y trabajo en equipo, nada es imposible!"</p>
-            </div>
-            <div class="enlacesFooter">
-                <div class="red github">
-                    <a href="https://github.com/Rokorider/Comparador-InazumaEleven">
-                    <img src="../img/redes/github-svgrepo-com.svg" alt="Enlace GitHub" class="githubImg">
-                    </a>
-                </div>
-                <div class="red">
-                    <a href="https://www.inazuma.jp/victory-road/en/"><img src="../img/logo/inazuma_japan.webp" alt="Enlace página oficial"></a>
-                </div>
-            </div>
-            <div class="copy">© 2024 Todos los derechos reservados.</div>
+    <div class="footer">
+        <div class="logoFooter">
+            <a href="#"><img src="../img/logo/InazumaElevenComparadorLogo2.png" alt="Logo Inazuma Comparador" class="logoImg"></a>
         </div>
-    </footer>
+        <div class="textoFooter">
+            <p id="textoFooter">"¡Con determinación y trabajo en equipo, nada es imposible!"</p>
+        </div>
+        <div class="enlacesFooter">
+            <div class="red github">
+                <a href="https://github.com/Rokorider/Comparador-InazumaEleven">
+                    <img src="../img/redes/github-svgrepo-com.svg" alt="Enlace GitHub" class="githubImg">
+                </a>
+            </div>
+            <div class="red">
+                <a href="https://www.inazuma.jp/victory-road/en/"><img src="../img/logo/inazuma_japan.webp" alt="Enlace página oficial"></a>
+            </div>
+        </div>
+        <div class="copy">© 2024 Todos los derechos reservados.</div>
+    </div>
+</footer>
