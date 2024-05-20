86951b0814c609f3c630ae288f89a6de3c7d7aae
ggdsrll <hugoestudiando1@gmail.com>
2024-05-20 11:11:59 +0200

    Added some styles and images

--- usuario/comentarios.php
+++ usuario/comentarios.php
@@ -64,7 +64,7 @@ if (!isset($_SESSION['usuario'])) {
         </div>
 
         <div class="tituloComent">
-            <h1>Comentarios</h1>
+            <h1>Futblog</h1>
         </div>
 
         <div class="comentarios" id="comentarios"></div>
